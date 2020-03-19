5c596eca38d85296431601317daba007bbf672cf
apple <lhong2@ualberta.ca>
2020-03-18 18:01:30 -0600

    init

--- sockets.py
+++ sockets.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect, jsonify
 from flask_sockets import Sockets
 import gevent
 from gevent import queue
@@ -65,23 +65,66 @@ def set_listener( entity, data ):
     ''' do something with the update ! '''
 
 myWorld.add_set_listener( set_listener )
-        
+
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
+
+clients = []
+
+def send_all(obj):
+    for client in clients:
+        client.put(obj)
+
+def send_all_json(obj):
+    send_all(json.dumps(obj))
+
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect("/static/index.html")
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
-    return None
+    try:
+        while True:
+            data = ws.receive()
+            print("WS RECV: {}".format(data))
+            if data:
+                packet = json.loads(data)
+                send_all_json(packet)
+                for entity in packet:
+                    myWorld.set(entity, packet[entity])
+                send_all_json(packet)
+            else:
+                break
+    except Exception as e:
+        raise e
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn(read_ws, ws, client)
+    try:
+        while True:
+            data = client.get()
+            ws.send(data)
+    except Exception as e:
+        raise e
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
@@ -99,23 +142,28 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    updateStuff = flask_post_json()
+    for key,value in updateStuff.items():
+        myWorld.update(entity, key, value)
+    return flask.Response(response=json.dumps(myWorld.get(entity)), content_type="application/json")
+
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return flask.Response(response=json.dumps(myWorld.world()), content_type="application/json")
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return flask.Response(response=json.dumps(myWorld.get(entity)), content_type="application/json")
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return flask.Response(response="", content_type="application/json")
 
 
 
